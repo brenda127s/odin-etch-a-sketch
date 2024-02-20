797dc75f02db8123b119cbabef77d22ef758ce95
brenda127s <brendaserra100@gmail.com>
2024-02-20 15:27:57 -0800

    js updated

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 const containerDiv = document.querySelector("#container");
 
-function makeRows(rows, columns) {
+function makeGrid(rows, columns) {
+    // check for existing button to remove if played already //
+    while (document.querySelector("button") !== null) {
+        document.querySelector("button").remove();
+    }
+   // create the grid //
     containerDiv.style.setProperty("--grid-rows", rows);
     containerDiv.style.setProperty("--grid-columns", columns);
-   // console.log(containerDiv);
-   // console.log(rows);
-   // console.log(columns);
+    containerDiv.style.width = "960px";
+    containerDiv.style.overflow = "hidden";
     for (i=0; i < (rows * columns); i++) {
         let square = document.createElement("div");
-      //  square.innerText = (i + 1); //labels the squares. remove when not needed//
+        square.style.minHeight = "0";
+        square.style.minWidth = "0";
+        square.style.overflow = "hidden";
         containerDiv.appendChild(square).className = "grid-item";
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = "black";
-            console.log(square);
         })
     }
+    createButton();
 }
-makeRows(16, 16);
+
+function createButton() {
+    const buttonDiv = document.querySelector("#buttonDiv");
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Reset Grid!";
+    resetButton.style.margin = "20px";
+    buttonDiv.appendChild(resetButton);
+
+    // add event listener to button and prompt user/reset grid/throw error > 100 //
+    resetButton.addEventListener('click', () => {
+        document.querySelectorAll(".grid-item").forEach(e => e.remove());
+        let userGridInput = prompt("Please enter the number of grid squares per side (Max: 100.): ");
+        if (userGridInput > 100) {
+            alert("ERROR! You specified a grid size larger than the max of 100.");
+            return;
+        }
+        rows = userGridInput;
+        columns = userGridInput;
+        makeGrid(rows, columns);
+    })
+}
+makeGrid(16, 16);
